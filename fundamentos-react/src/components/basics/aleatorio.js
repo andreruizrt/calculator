5c0c165d7def102a81c3aa893fee0ab731af37d2
Andre Ruiz <36556771+andreruizrt@users.noreply.github.com>
2021-03-04 23:53:29 -0300

    delete learning folder

--- fundamentos-react/src/components/basics/aleatorio.js
+++
@@ -1,9 +0,0 @@
-export default function NumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-export function corAleatoria() {
-    let bg_colour = Math.floor(Math.random() * 16777215).toString(16);
-    return bg_colour = "#" + ("000000" + bg_colour).slice(-6);
-}
-    
